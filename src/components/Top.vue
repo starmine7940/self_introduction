9a09c6d21a53c836d2d22a9e879b6a19a491b92a
starmine7940 <starmine7940@gmail.com>
2021-09-12 21:14:39 +0900

    とりあえず雛形は完成？

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,52 +1,24 @@
 <template>
-  <div class="output">
-    <div class="content" v-for="i in 6" :key=i>
-    <h3> {{ name }} </h3>
-    <p> {{ text }} </p>
-  </div>
-  </div>
+  	<div class="wrapper">
+    	<h1>Top</h1>
+  	</div>
 </template>
 
 <script>
-export default {
-  name: 'Output',
-  data () {
-    return {
-      name: 'トップ',
-      text: 'このような意図で作成しました。ここをこのように工夫しました。ここが難しかったです。今後はこうゆう機能を追加したいです。'.repeat(3)
-    }
-  }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-div.output {
-  text-align: center;
-  animation: fadein 3s forwards;
+.wrapper {
+	text-align: center;
+	animation: fadein 3s forwards;
 }
 
 @keyframes fadein {
-	0% {opacity: 0}
-	100% {opacity: 1}
+	0% {
+		opacity: 0;
+	}
+	100% {
+		opacity: 1;
+  }
 }
-
-div.content {
-  background-color: #eee;
-  display: inline-block;
-  margin: 10px;
-  width: 30%;
-}
-
-h3 {
-  color: white;
-}
-
-p {
-  width: 90%;
-  display: inline-block;
-  text-align: left;
-}
-
 </style>
